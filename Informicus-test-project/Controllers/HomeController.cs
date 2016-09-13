2cc5b82191cd81e0e801cbb1989f45773c91d657
crimean-s <insarabuz@gmail.com>
2016-09-14 00:53:41 +0300

    Добавлен класс UsersGroupsViewModel и реализован вывод через частичное представление

--- Informicus-test-project/Controllers/HomeController.cs
+++ Informicus-test-project/Controllers/HomeController.cs
@@ -4,16 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Informicus_test_project.Models;
+using Domain;
+using System.Configuration;
+using Domain.Entities;
+using Informicus_test_project.Models.ViewModels;
 
 namespace Informicus_test_project.Controllers
 {
+    
     public class HomeController : Controller
     {
+
+        EFDbContext data = new EFDbContext(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
+        // GET: Users Groups
+        public ActionResult UsersGroups()
+        {
+            IEnumerable<User> users = data.Users.Where(x => x.UserNickname.Length > 0);
+            IEnumerable<Group> groups = data.Groups.Where(x => x.GroupName.Length > 0);
+
+            UsersGroupViewModel ugvm = new UsersGroupViewModel
+            {
+                Users = users,
+                Groups = groups
+            };
+
+            return View(ugvm);
+        }
+
+
+
 
         // GET: Users
         public ActionResult Users()
---
+++ Informicus-test-project/Models/ViewModels/UsersGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+using Domain.Entities;
+
+namespace Informicus_test_project.Models.ViewModels
+{
+    public class UsersGroupViewModel
+    {
+        public IEnumerable<User>  Users { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+    }
+}
